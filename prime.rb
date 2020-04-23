7cdba439d93c51b0a5bbe9e01cb46b049262674c
Tony Rim <duc_thinh_03@yahoo.com>
2020-04-23 21:43:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,5 @@
 def prime?(number)
- prime_num = (2..number-1).none? {|divisor| number % divisor == 0}
-  if prime_num 
-    true 
-  else 
-    false 
-  end 
+  returm false if number < 2
+ (2..number-1).none? {|divisor| number % divisor == 0}
+  
 end 
